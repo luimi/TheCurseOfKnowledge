9432375777d4a8b8c33e240de02abab6d92d60b6
luis meza <luimi@live.com>
2022-10-23 17:30:58 -0500

    validacion de categorias

--- src/app/manager/categories/categories.component.ts
+++ src/app/manager/categories/categories.component.ts
@@ -4,6 +4,7 @@ import { EventEmitter } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ConfirmDialogComponent } from '../confirm-dialog/confirm-dialog.component';
 import { UtilsService } from 'src/app/utils.service';
+import { MatSnackBar } from '@angular/material';
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
@@ -13,28 +14,46 @@ export class CategoriesComponent implements OnInit {
   @Input() categories = [];
   @Output() refresh = new EventEmitter();
   name;
-  constructor(public dialog: MatDialog, public utils: UtilsService) { }
+  constructor(public dialog: MatDialog, public utils: UtilsService, private snackBar: MatSnackBar) { }
 
   ngOnInit() {
   }
-  async save(){
-    if(this.name){
-      const Category = Parse.Object.extend('Category');
-      let category = new Category();
-      category.set('name',this.name);
-      category.setACL(await this.utils.getAdminACL());
-      await category.save();
-      this.name = undefined;
-      this.refresh.emit();
+  async save() {
+    if (!this.name) {
+      this.showSnackBar("Debe escribir un nombre");
+      return
     }
+    if (this.isRegistered(this.name)) {
+      this.showSnackBar("Existe una categoria con el mismo nombre");
+      return
+    }
+    const Category = Parse.Object.extend('Category');
+    let category = new Category();
+    category.set('name', this.name);
+    category.setACL(await this.utils.getAdminACL());
+    await category.save();
+    this.name = undefined;
+    this.refresh.emit();
   }
-  async delete(category){
-    const dialogRef = this.dialog.open(ConfirmDialogComponent,{data:{message:'Do you want to delete this category?'}});
-    dialogRef.afterClosed().subscribe( async result => {
-      if(result){
+
+  async delete(category) {
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, { data: { message: 'Do you want to delete this category?' } });
+    dialogRef.afterClosed().subscribe(async result => {
+      if (result) {
         await category.destroy();
         this.refresh.emit();
       }
     });
+  }
+  isRegistered(text) {
+    let category = this.categories.find(category => {
+      if (category.get("name").toLowerCase() === text.toLowerCase()) {
+        return category;
+      }
+    });
+    return category !== undefined;
+  }
+  showSnackBar(message) {
+    this.snackBar.open(message, null, { duration: 3000 });
   }
 }
