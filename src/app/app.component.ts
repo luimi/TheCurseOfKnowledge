42439a2329c174a93a35853a95650d3ed856093b
luis meza <luimi@live.com>
2022-11-25 09:15:59 -0500

    redirigir al logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
+import { Router } from '@angular/router';
 import Parse from 'parse';
 import { LoginComponent } from './login/login.component';
 @Component({
@@ -10,7 +11,7 @@ import { LoginComponent } from './login/login.component';
 export class AppComponent {
   title = 'TheCurseOfKnowledge';
   isLogedIn = false;
-  constructor(public dialog: MatDialog){
+  constructor(public dialog: MatDialog, private router: Router){
     Parse.initialize("tCbO3YhTW1fNFp6tC6rIy2IqR1dL6unVs4NsDx7Q", 
     "ZRTekpfa2Aacehyd2c3AVsMZNirugnTGI7DDaV7O");
     Parse.serverURL = 'https://parseapi.back4app.com/';
@@ -25,6 +26,7 @@ export class AppComponent {
   async logout(){
     await Parse.User.logOut();
     this.isLogedIn = Parse.User.current()!=undefined;
+    this.router.navigateByUrl("/");
   }
   github(){
     window.open("https://github.com/luimi/TheCurseOfKnowledge", "_blank");
